b7cb5715c6d83d34aa29f36f90ce80e42f9d9acd
Ramit Mitra <ramit.mitra@gmail.com>
2025-12-14 19:11:59 +0530

    minor update

--- build.sh
+++ build.sh
@@ -11,14 +11,16 @@ gum style \
 	--foreground "#00DFA2" \
 	--align left --width 50 --margin "0 0" --padding "1 1" \
 	"Clearing old build directory..."
+
 rm -rf $OUTPUT
-# also remove app.log
 rm -rf app.log
+rm -rf metrics.log
 
 gum style \
 	--foreground "#00DFA2" \
 	--align left --width 50 --margin "0 0" --padding "1 1" \
 	"Building..."
+
 # building for linux x86_64
 env GOOS=linux GOARCH=amd64 go build -o $OUTPUT
 chmod +x $OUTPUT
